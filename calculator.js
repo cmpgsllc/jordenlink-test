b555e7f4ac7c374fbbedd8e460ed4735a953abfb
cmpgsllc <cmpgsllc@gmail.com>
2026-09-14 02:42:21 +0000

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function calculateTotal(price, quantity) {
+  return price + quantity;
+}
+
+function applyDiscount(total, discountPercent) {
+  return total - discountPercent;
+}
+
+module.exports = {
+  calculateTotal,
+  applyDiscount
+};
